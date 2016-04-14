fc2bd7ce0b57f0ab67b1b3e5106e942c496a0cc8
Martijn Jacobs <martijn@devopsconsulting.nl>
2016-04-14 16:45:42 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,24 @@ from setuptools import find_packages, setup
 pkgname = "vdt.versionplugin.buildout"
 
 setup(name=pkgname,
-      version="0.0.1",
+      version="0.0.2",
       description="Version Increment Plugin that builds with debianize",
       author="CSI",
       author_email="csi@avira.com",
       maintainer="CSI",
       maintainer_email="csi@avira.com",
-      packages=find_packages(exclude=['vdt.versionplugin.buildout']),
+      packages=find_packages(),
       include_package_data=True,
       namespace_packages=['vdt', 'vdt.versionplugin'],
       zip_safe=True,
       install_requires=[
           "setuptools",
-          "mock",
           "vdt.version",
           "vdt.versionplugin.default",
       ],
       entry_points={},
-      )
+      extras_require={
+          'test': [
+              'mock',
+          ]
+      })
